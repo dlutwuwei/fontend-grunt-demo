a48a3383b511a22b93bb73933684a014a698a97c
dlutwuwei <dlutwuwei@qq.com>
2015-11-21 22:58:06 +0800

    modify gruntfile for livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,6 @@ module.exports = function(grunt) {
 
 
 	var lrPort = 35729;
-	// 使用connect-livereload模块，生成一个与LiveReload脚本
-	// <script src="http://127.0.0.1:35729/livereload.js?snipver=1" type="text/javascript"></script>
-	var lrSnippet = require('connect-livereload')({
-		port: lrPort
-	});
-
-	var serveStatic = require('serve-static');
-	var serveIndex = require('serve-index');
-	var lrMiddleware = function(connect, options, middlwares) {
-		return [
-			lrSnippet,
-			// 静态文件服务器的路径 原先写法：connect.static(options.base[0])
-			serveStatic(options.base[0]),
-			// 启用目录浏览(相当于IIS中的目录浏览) 原先写法：connect.directory(options.base[0])
-			serveIndex(options.base[0])
-		];
-	};
 
 	// 项目配置.
 	grunt.initConfig({
@@ -81,7 +64,7 @@ module.exports = function(grunt) {
 			livereload: {
 				options: {
 					open: true, //自动打开网页 http://
-					middleware: lrMiddleware,
+					livereload:lrPort,
 				}
 
 			}
