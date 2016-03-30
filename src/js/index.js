9f8d9c301336217220c17570008882b20d98975b
dlutwuwei <dlutwuwei@qq.com>
2016-03-30 14:07:12 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-$(function(){
+$(function() {
 
 	var panels = $('.slider .slider-panel');
 	var dots = $('.slider-nav .item');
-	dots.hover(function(evt){
+	dots.hover(function(evt) {
 		dots.removeClass('select')
 		$(this).addClass('select')
 		var index = $(this).attr('index');
@@ -10,9 +10,9 @@ $(function(){
 		$(panels[index]).addClass('show');
 	});
 
-	function turn(){
+	function turn() {
 		var pos = 0;
-		return setInterval(function(){
+		return setInterval(function() {
 			panels.removeClass('show');
 
 		})
@@ -20,9 +20,9 @@ $(function(){
 
 	var servs = $('.service-list .service');
 	var covers = $('.service-list .cover');
-	servs.hover(function(evt){
+	servs.hover(function(evt) {
 		covers.hide();
 		var el = $(this).find('.cover').show();
-		
+
 	});
 })
